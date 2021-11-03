572c0251be0b05a671cd7650e8b8017d3f1151e9
AleksandrVasilko <avasilko7@gmail.com>
2021-11-03 22:26:48 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const listItemEl = document.createElement('li');
+  listItemEl.classList.add('item');
+  listItemEl.textContent = ingredients[i];
+  elements.push(listItemEl);
+}
+
+const ulLislEl = document.querySelector('#ingredients');
+ulLislEl.append(...elements);
