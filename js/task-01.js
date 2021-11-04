8bd9ce79d4cbb1e938fc2960bfd29701593f47c1
AleksandrVasilko <avasilko7@gmail.com>
2021-11-04 20:22:46 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 const listItems = document.querySelectorAll('.item');
-console.log("Number of categories: " + listItems.length);
+console.log(`Number of categories: ${listItems.length}`);
 console.log(' ');
 
 for (let i = 0; i < listItems.length; i += 1) {
@@ -7,7 +7,7 @@ for (let i = 0; i < listItems.length; i += 1) {
     const titleEl = listItems[i].querySelector('h2').textContent;
     const itemEl = listItems[i].querySelectorAll('li');
     
-    console.log("Category: " + titleEl);
-    console.log("Elements: " + itemEl.length);
+    console.log(`Category: ${titleEl}`);
+    console.log(`Elements: ${itemEl.length}`);
     console.log(' ');
 }
