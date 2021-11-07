9c06e05ad4002a1877deccfaa68b5e47452a2765
AleksandrVasilko <avasilko7@gmail.com>
2021-11-07 23:36:43 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    const formElemens = event.currentTarget.elements;
+
+    const email = formElemens.email.value;
+
+    const password = formElemens.password.value;
+
+    if (email === '' || password === '') {
+        alert('все поля должны быть заполнены');
+    } else {
+
+        const formData = new FormData(event.currentTarget)
+
+        formData.forEach((value, name) => {
+            console.log(name, value)
+        });
+        
+        form.reset();
+    }
+}
